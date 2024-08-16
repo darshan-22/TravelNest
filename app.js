64cd25872a7f638dd924d215c141f92b40c5769a
darshan-22 <darshannn2003@gmail.com>
2024-08-16 11:41:45 +0530

    added index route

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const app =express();
+const app = express();
 const mongoose  = require("mongoose");
+const Listing  = require("./models/listing.js")
+const path  = require("path");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/travelnest";
 
@@ -12,10 +14,38 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,"views"));
+
 app.get("/", (req,res) => {
     res.send("Welcome to your home page!");
 })
 
+
+// INDEX ROUTE
+app.get("/listings" , async (req,res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings});
+})
+
+
+
 app.listen(8080, () => {
     console.log("Server has started!")
 })
+
+
+// app.get("/testlisting", async (req,res) => {
+//     //creating a document to be inserted in the db
+//     let sampleListing = await new Listing({
+//         title:"Premium Villa",
+//         description :"Luxurious duplex villa with plush greenery and night life.",
+//         price: 10000,
+//         location:"Calangute , Goa",
+//         country:"India",
+//     });
+
+//    await sampleListing.save();
+//    console.log("sample listing was saved");
+//    res.send("Success Testing");
+// })
--- models/listing.js
+++ models/listing.js
@@ -1,13 +1,20 @@
 const mongoose = require("mongoose");
 
 let listingSchema = new mongoose.Schema({
-    title:String,
+    title: {
+        type:String,
+        required:true,
+    },
     description:String,
-    image:String,
+    image: {
+        type: String,
+        default: "https://images.unsplash.com/photo-1531971589569-0d9370cbe1e5?q=80&w=1481&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        set: (v) => v === "" ? "https://images.unsplash.com/photo-1531971589569-0d9370cbe1e5?q=80&w=1481&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+    },
     price:Number,
     location:String,
     country:String,
 });
 
 const Listing = new mongoose.model("Listing", listingSchema);
-modules.export = Listing;
+module.exports = Listing;
