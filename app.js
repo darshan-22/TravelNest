c54d2f7e151b5e7607eed244dca1df1f41c70493
darshan-22 <darshannn2003@gmail.com>
2024-08-28 18:39:14 +0530

    added wrapAsync and ExpressError to all routes

--- app.js
+++ app.js
@@ -1,87 +1,123 @@
 const express = require("express");
 const app = express();
-const mongoose  = require("mongoose");
-const Listing  = require("./models/listing.js")
-const path  = require("path");
-const methodOverride  = require("method-override");
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const expressError = require("./utils/ExpressError.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/travelnest";
 
-main().then(() => {
+main()
+  .then(() => {
     console.log("connected to db");
-}).catch(err => console.log(err));
+  })
+  .catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname,"views"));
-app.use(express.urlencoded({extended:true}));
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine('ejs',ejsMate);
-app.use(express.static(path.join(__dirname,"/public")))
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.get("/", (req,res) => {
-    res.send("Welcome to your home page!");
-})
-
+app.get("/", (req, res) => {
+  res.send("Welcome to your home page!");
+});
 
 // INDEX ROUTE
-app.get("/listings" , async (req,res) => {
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
-    res.render("listings/index.ejs", {allListings});
-})
+    res.render("listings/index.ejs", { allListings });
+  })
+);
 
 //NEW ROUTE
-app.get("/listings/new" , async (req,res) => {
+app.get(
+  "/listings/new",
+  wrapAsync(async (req, res) => {
     res.render("listings/new.ejs");
-})
+  })
+);
 
 //SHOW ROUTE
-app.get("/listings/:id", async (req,res) => {
-    let {id}  = req.params;
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
     const listing = await Listing.findById(id);
-    res.render("listings/show.ejs" , {listing});
-})
+    res.render("listings/show.ejs", { listing });
+  })
+);
 
 //CREATE ROUTE
-app.post("/listings", async (req,res) => {
-    const newListing  = new Listing(req.body.listing);
+app.post(
+  "/listings",
+  wrapAsync(async (req, res, next) => {
+    if (!req.body.listing) {
+      throw new ExpressError(400, "Send valid data for listing!");
+    }
+    const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-})
+  })
+);
 
 //EDIT ROUTE
-app.get("/listings/:id/edit", async (req,res) => {
-    let {id}  = req.params;
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
     const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", {listing})
-})
+    res.render("listings/edit.ejs", { listing });
+  })
+);
 
 //UPDATE ROUTE
-app.put("/listings/:id", async(req,res) => {
-    let {id}  = req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+app.put(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    if (!req.body.listing) {
+      throw new ExpressError(400, "Send valid data for listing!");
+    }
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
-})
+  })
+);
 
 //DELETE ROUTE
-app.delete("/listings/:id" , async (req,res) => {
-    let {id} = req.params;
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
+  })
+);
 
-})
+//CHECKS ALL THE ROUTES ABOVE AND IF NO ROUTE IS MATCHED ABOVE , THIS WILL BE EXECUTED
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found"));
+});
 
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something went wrong" } = err;
+  res.status(statusCode).send(message);
+});
 
 app.listen(8080, () => {
-    console.log("Server has started!")
-})
-
-
+  console.log("Server has started!");
+});
 
 // BEFORE CREATING ANY ROUTES , A SAMPLE TESTING CODE TO SEE IF DATA IS BEING INSERTED IN THE DATABASE.
 // app.get("/testlisting", async (req,res) => {
